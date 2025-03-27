b40edf023e2cea251bd6c2662d2c5cf16ec80dff
Paula <paulapierina96@gmail.com>
2025-03-27 13:33:12 -0500

    refactor base form

--- src/Features/BusinessForm/formFields.js
+++ src/Features/BusinessForm/formFields.js
@@ -1,7 +1,71 @@
 export const formFields = [
-  { name: "businessName", label: "Nombre del negocio", type: "text", validation: { required: "El nombre es obligatorio" } },
-  { name: "businessAge", label: "Antigüedad del negocio (años)", type: "number", validation: { required: "Campo obligatorio" } },
-  { name: "employmentStatus", label: "Situación laboral", type: "text", validation: { required: "Campo obligatorio" } },
-  // { name: "annualGoals", label: "Metas anuales del año previo", type: "text", validation: { required: "Campo obligatorio" } },
-  { name: "website", label: "Link a página web", type: "url", validation: { pattern: { value: /https?:\/\/.+/, message: "URL inválida" } } },
+  {
+    name: "userProfile",
+    label: "¿Cuál es tu perfil de ingreso?",
+    type: "radioButton",
+    options: [{
+      label: 'Independiente',
+      value: 'independent',
+      icon: 'type'
+    },
+    {
+      label: 'Dependiente',
+      value: 'Dependent',
+      icon: 'type'
+    }],
+    validation: { required: "Selecciona uno es obligatorio" }
+  },
+  {
+    name: "incomeStream",
+    label: "¿Cuál es tu fuente principal de ingresos?",
+    type: "select",
+    options: [{
+      label: 'Negocio propio',
+      value: 'ownBusiness',
+    },
+    {
+      label: 'Marca Personal',
+      value: 'personalBrand',
+    }],
+    validation: { required: "Selecciona es obligatorio" }
+  },
+  {
+    name: "businessAge",
+    label: "¿Cuántos años tiene tu negocio?",
+    type: "number",
+    validation: { required: "Campo obligatorio" }
+  },
+  {
+    name: "businessEvolution",
+    label: "¿Tu negocio ha tenido un crecimiento constante en los últimos años o ha enfrentado desafíos significativos?",
+    type: "text",
+    textLimit: '200',
+    validation: { required: "Campo obligatorio" }
+  },
+  {
+    name: "incomeAverage",
+    label: "¿Cuál es el promedio de ingresos mensuales de tu negocio?",
+    type: "range",
+    min: 3000,
+    max: 50000,
+    validation: { required: "Campo obligatorio" }
+  },
+  {
+    name: "businessPercentage",
+    label: "¿Qué porcentaje de sus ingresos proviene de su negocio?",
+    type: "text",
+    validation: { required: "Campo obligatorio" }
+  },
+  {
+    name: "extraIncomePercentage",
+    label: "¿Qué porcentaje proviene de otras fuentes?",
+    type: "text",
+    validation: { required: "Campo obligatorio" }
+  },
+  {
+    name: "website",
+    label: "¿Tienes un página web?",
+    type: "url",
+    validation: { pattern: { value: /https?:\/\/.+/, message: "URL inválida" } }
+  },
 ];
