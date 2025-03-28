ab9642167aced31cc7a00b504d58aef2c3d1da8f
Paula <paulapierina96@gmail.com>
2025-03-28 01:56:49 -0500

    comment

--- src/pages/AboutUser.jsx
+++ src/pages/AboutUser.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 import { Typography, Button } from "@mui/material";
 import MainLayout from "../layouts/MainLayout";
 import BusinessForm from "../Features/BusinessForm/default";
